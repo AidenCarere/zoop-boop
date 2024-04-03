bc4e41cf96c5c0c2c62baa3d3e9c7c7ed3dbeab2
AidenCarere <aidencarere@gmail.com>
2024-04-03 14:42:10 -0700

    Package.json configured, client folder added and server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// Middlewares
+app.use(express.json()); // For parsing application/json
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
+// Default Route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
